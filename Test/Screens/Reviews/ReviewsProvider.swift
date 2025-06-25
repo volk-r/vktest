47d06e6b3cea3f7528e0c08c759ff5c2a3632fa6
Roman Romanov <rromanov.vk@gmail.com>
2025-06-26 00:36:12 +0500

    fixed scroll

--- Test/Screens/Reviews/ReviewsProvider.swift
+++ Test/Screens/Reviews/ReviewsProvider.swift
@@ -25,18 +25,26 @@ extension ReviewsProvider {
     }
 
     func getReviews(offset: Int = 0, completion: @escaping (GetReviewsResult) -> Void) {
-        guard let url = bundle.url(forResource: "getReviews.response", withExtension: "json") else {
-            return completion(.failure(.badURL))
+        DispatchQueue.global(qos: .background).async {
+            guard let url = self.bundle.url(forResource: "getReviews.response", withExtension: "json") else {
+                return self.completeOnMainThread(completion, result: .failure(.badURL))
+            }
+            
+            // Симулируем сетевой запрос - не менять
+            usleep(.random(in: 100_000...1_000_000))
+            
+            do {
+                let data = try Data(contentsOf: url)
+                self.completeOnMainThread(completion, result: .success(data))
+            } catch {
+                self.completeOnMainThread(completion, result: .failure(.badData(error)))
+            }
         }
-
-        // Симулируем сетевой запрос - не менять
-        usleep(.random(in: 100_000...1_000_000))
-
-        do {
-            let data = try Data(contentsOf: url)
-            completion(.success(data))
-        } catch {
-            completion(.failure(.badData(error)))
+    }
+    
+    private func completeOnMainThread(_ completion: @escaping (GetReviewsResult) -> Void, result: GetReviewsResult) {
+        DispatchQueue.main.async {
+            completion(result)
         }
     }
 
